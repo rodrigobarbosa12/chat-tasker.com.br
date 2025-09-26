bd0f850043313dd31a337ee9e6332a8e927e28b1
Rodrigo Barbosa <rbarbosa@mtrix.com.br>
2025-09-26 11:37:06 -0300

    Chat Task Manager

--- src/infrastructure/providers/index.ts
+++ src/infrastructure/providers/index.ts
@@ -1,7 +1,7 @@
-export { defaultLocale, langs, nextIntl, type Locale } from "./intl";
+export { defaultLocale, langs, nextIntl, type Locale } from './intl'
 export {
   defaultDictionary,
   defaultDictionaryDateFns,
   type Dictionary,
-} from "./intl/dictionaries";
-export { useAuth, useChangeLanguage, useDrawer } from "./zustand/store";
+} from './intl/dictionaries'
+export { useAuth, useChangeLanguage, useDrawer } from './zustand/store'
